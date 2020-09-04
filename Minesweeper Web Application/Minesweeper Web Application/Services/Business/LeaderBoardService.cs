3f08ce6e2e8d18e0f4d13536111d8b0e8a276488
cmdeao <cmdeao16@gmail.com>
2020-09-04 12:48:57 -0500

    Adjusted various aspects of data insertion and retrieval for database. Created gameplay scoring. Updated highscore view. Created leader board scoring, insertion, and retrieval.

---
+++ Minesweeper Web Application/Minesweeper Web Application/Services/Business/LeaderBoardService.cs
@@ -0,0 +1,24 @@
+﻿using Minesweeper_Web_Application.Models;
+using Minesweeper_Web_Application.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Minesweeper_Web_Application.Services.Business
+{
+    public class LeaderBoardService
+    {
+        LeaderBoardDAO board = new LeaderBoardDAO();
+
+        public List<LeaderBoard> GetScores(List<LeaderBoard> inc)
+        {
+            return board.GetScores(inc);
+        }
+
+        public void InsertHighScore(UserModel user, decimal time)
+        {
+            board.InsertHighScore(user, time);
+        }
+    }
+}
